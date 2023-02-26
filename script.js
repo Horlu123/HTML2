5cf366d72d617f1f7a032418ab9f0011f451c609
Treasure Dev <treasurealekhojie@yahoo.com>
2023-02-26 21:02:10 +0000

    Reasigned fixed

--- script.js
+++ script.js
@@ -12,29 +12,28 @@ let totalUnits = 0;
 function calculateGPA() {
   var courseGradeInputs = document.getElementsByClassName("course-grade");
   var unitInputs = document.getElementsByClassName("unit");
-  // console.log(courseGradeInputs, unitInputs)
 
   for (let i = 0; i < courseGradeInputs.length; i++) {
     let grade = parseFloat(courseGradeInputs[i].value);
     let units = parseFloat(unitInputs[i].value);
     console.log(units)
 
-    // Check if the input values are valid numbers
-    // if (isNaN(grade) || isNaN(units)) {
-    //   alert("Please enter valid numeric values.");
-    //   return;
-    // }
+    Check if the input values are valid numbers
+    if (isNaN(grade) || isNaN(units)) {
+      alert("Please enter valid numeric values.");
+      return;
+    }
 
     totalGradePoints += grade * units;
     totalUnits += units;
     console.log(totalUnits)
   }
 
-  // Check if any input fields are empty
-  // if (totalUnits === 0) {
-  //   alert("Please enter values for all input fields.");
-  //   return;
-  // }
+  Check if any input fields are empty
+  if (totalUnits === 0) {
+    alert("Please enter values for all input fields.");
+    return;
+  }
 
   var gpa = totalGradePoints / totalUnits;
 
